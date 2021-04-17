605984a6d61e2e97562d020dc61d20d076493d5c
Steve-A-Dore <Steve.A.Dore@outlook.com>
2021-04-16 18:06:09 -0700

    steve commit for coins. Did all the author stuff. Issue with massive inconsistencies quality of author data. Will consult.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var apiKey = 'vIrkw0zTaB0xGuFESxisI1NuaqV5vJqz';
 var defaultCall = 'https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=';
+var defaultImage = 'https://demo.publishr.cloud/uploads/demo/books/493/edition/823/sale-test.png?1586175097';
 var display = true;
 requestData(apiKey);
 var $row = document.querySelector('#presentation-row');
@@ -7,8 +8,10 @@ var $rowAuthor = document.querySelector('#presentation-row-author');
 var $homeContainer = document.querySelector('.container');
 var $searchContainer = document.querySelector('.container-search');
 var $authorContainer = document.querySelector('.container-author-results');
-var $homeButton = document.getElementById('button2');
+var $homeButton = document.getElementById('button1');
+var $returnHomeButton = document.getElementById('button2');
 var $authorSearchButton = document.getElementById('authorSearchButton');
+var $categorySearchButton = document.getElementById('categorySearchButton');
 var $inputForm = document.getElementById('input-form');
 var displayAuthor = true;
 
@@ -31,6 +34,26 @@ function authorRequestData(event) {
   });
   $inputForm.reset();
   request.send(null);
+}
+
+function categoryRequestData(event) {
+  event.preventDefault();
+  var categoryName = generateCategorySearch($inputForm.search.value);
+  var categoryCall = `https://api.nytimes.com/svc/books/v3/lists.json?list=${categoryName.firstWord}-${categoryName.secondWord}&api-key=${apiKey}`;
+  var request = new XMLHttpRequest();
+  request.open('GET', categoryCall, true);
+  request.responseType = 'json';
+  request.addEventListener('load', function () {
+    // var booksResponse = request.response;
+    // var booksResponseObject = booksResponse.results; // this is the array here. In the other functions its another level down. Don't get confused by this.
+  });
+  request.send();
+}
+
+function generateCategorySearch(name) {
+  var categoryName = {}; name = name.split(' ');
+  categoryName.firstWord = name[0]; categoryName.secondWord = name[1];
+  return categoryName;
 }
 
 function generateFirstLastName(name) {
@@ -60,6 +83,7 @@ function displayChange() {
   if (display === true) {
     $homeContainer.setAttribute('class', 'container hidden');
     $searchContainer.setAttribute('class', 'container-search');
+    $authorContainer.setAttribute('class', 'container-author-results hidden');
     display = false;
   } else {
     $homeContainer.setAttribute('class', 'container');
@@ -79,19 +103,24 @@ function displayChangeAuthor() {
   }
 }
 
+// function renderCategoryEntry() {
+
+// }
+
 function renderAuthorEntry(entry) {
   var outerCard = document.createElement('div');
   var card = document.createElement('div');
   var firstRow = document.createElement('div');
   var secondRow = document.createElement('div');
-
+  var header = document.createElement('h3');
   var boldAuthor = document.createElement('b');
   var titleSpan = document.createElement('span');
   var authorSpan = document.createElement('span');
   var titleSpanElement = document.createElement('span');
   var titleParagraphElement = document.createElement('p');
-
+  var image = document.createElement('img');
   var cardTextHolder = document.createElement('div');
+  var bookImage = entry.book_image === undefined ? defaultImage : entry.book_image;
 
   var authorSlot = document.createTextNode('Author: ');
   var titleEntry = document.createTextNode('Title:  ');
@@ -115,10 +144,13 @@ function renderAuthorEntry(entry) {
   firstRow.setAttribute('class', 'row display');
   secondRow.setAttribute('class', 'row display');
   authorSpan.setAttribute('class', 'author-font-size');
-
+  header.setAttribute('class', 'card-header');
+  image.setAttribute('src', bookImage);
   cardTextHolder.setAttribute('class', 'card-text-holder');
   titleSpan.setAttribute('class', 'title');
 
+  card.appendChild(header);
+  card.appendChild(image);
   firstRow.appendChild(boldAuthor);
   firstRow.appendChild(authorSpan);
   cardTextHolder.appendChild(firstRow);
@@ -142,7 +174,7 @@ function renderEntry(entry) {
   var titleParagraphElement = document.createElement('p');
   var image = document.createElement('img');
   var cardTextHolder = document.createElement('div');
-  var bookImage = entry.book_image;
+  var bookImage = entry.book_image === undefined ? defaultImage : entry.book_image;
 
   var numberHeading = document.createTextNode('#' + entry.rank);
   var authorSlot = document.createTextNode('Author: ');
@@ -187,4 +219,5 @@ function renderEntry(entry) {
 
 $homeButton.addEventListener('click', displayChange);
 $authorSearchButton.addEventListener('click', authorRequestData);
-// $categorySearchButton.addEventListener('click', )
+$categorySearchButton.addEventListener('click', categoryRequestData);
+$returnHomeButton.addEventListener('click', displayChange);
