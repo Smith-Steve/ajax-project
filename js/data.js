a92be41e70d850c6005c8671948b456b2928067a
Smith-Steve <Steve.A.Dore@outlook.com>
2021-04-21 22:13:07 -0700

    adding changes made to js/data file.

--- js/data.js
+++ js/data.js
@@ -1,1 +1,18 @@
 /* exported data */
+var data = {
+  view: 'favorited-book',
+  entries: [],
+  editing: null,
+  nextEntryId: 1
+};
+
+var previousToDoJSON = localStorage.getItem('favorited-books');
+
+if (previousToDoJSON !== null) {
+  data = JSON.parse(previousToDoJSON);
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var transformToJSON = JSON.stringify(data);
+  localStorage.setItem('favorited-books', transformToJSON);
+});
